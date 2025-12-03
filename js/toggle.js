bc749adf63c1b64f644055d721c61ef41ddf5142
SHO KANEKO <skaug985@yahoo.co.jp>
2025-12-03 19:07:36 +0900

    daily

--- js/toggle.js
+++ js/toggle.js
@@ -1,7 +1,9 @@
 const btn1to20 = document.getElementById("btn-about1-20");
 const btn21to40 = document.getElementById("btn-about21-40");
+const btn41to60 = document.getElementById("btn-about41-60");
 const block1to20 = document.getElementById("about1-20");
 const block21to40 = document.getElementById("about21-40");
+const block41to60 = document.getElementById("about41-60");
 
 // ボタン1: about1-20 を表示/非表示
 btn1to20.addEventListener("click", () => {
@@ -11,4 +13,9 @@ btn1to20.addEventListener("click", () => {
 // ボタン2: about21-40 を表示/非表示
 btn21to40.addEventListener("click", () => {
   block21to40.classList.toggle("hidden");
+});
+
+// ボタン2: about41-40 を表示/非表示
+btn41to60.addEventListener("click", () => {
+  block41to60.classList.toggle("hidden");
 });
